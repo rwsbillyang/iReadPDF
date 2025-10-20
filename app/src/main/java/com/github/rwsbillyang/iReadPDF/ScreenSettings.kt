5765854b65e1c863c2ffa709f1123d6e8284253a
BillYang <rwsbillyang@qq.com>
2025-10-20 15:19:06 +0800

    Add snack bar prompt when clear cached pages

--- app/src/main/java/com/github/rwsbillyang/iReadPDF/ScreenSettings.kt
+++ app/src/main/java/com/github/rwsbillyang/iReadPDF/ScreenSettings.kt
@@ -2,11 +2,23 @@ package com.github.rwsbillyang.iReadPDF
 
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -29,79 +41,94 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "se
 fun SettingsScreen(paddingValues: PaddingValues) {
     val viewModel: MyViewModel = LocalViewModel.current
     val ctx =  LocalContext.current
+    val snackState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
     val groupName = stringResource(id = R.string.settings)
-    PrefsScreen(dataStore = LocalContext.current.dataStore, modifier = Modifier.padding(paddingValues)) {
+    Box(Modifier.padding(paddingValues).fillMaxSize(), Alignment.Center){
 
-        prefsGroup(groupName) {
-            prefsItem{
-                CheckBoxPref(key = AppConstants.SettingsKey.EnterBookDirectly,
-                    title = stringResource(id = R.string.enter_book),
-                    summary = stringResource(id = R.string.enter_book_desc),
-                    textColor = MaterialTheme.colorScheme.primary,
-                    defaultChecked = false,
-                    onCheckedChange = {v-> viewModel.enterBookDirectly = v},
-                    enabled = true)
-            }
+        PrefsScreen(dataStore = LocalContext.current.dataStore) {
+            prefsGroup(groupName) {
+                prefsItem{
+                    CheckBoxPref(key = AppConstants.SettingsKey.EnterBookDirectly,
+                        title = stringResource(id = R.string.enter_book),
+                        summary = stringResource(id = R.string.enter_book_desc),
+                        textColor = MaterialTheme.colorScheme.primary,
+                        defaultChecked = false,
+                        onCheckedChange = {v-> viewModel.enterBookDirectly = v},
+                        enabled = true)
+                }
 
-            prefsItem{
-                CheckBoxPref(key = AppConstants.SettingsKey.DisableMovePdf,
-                    title = stringResource(id = R.string.disable_move_pdf),
-                    summary = stringResource(id = R.string.disable_move_pdf_desc),
-                    textColor = MaterialTheme.colorScheme.primary,
-                    defaultChecked = true,
-                    onCheckedChange = {v-> viewModel.disableMovePdf = v},
-                    enabled = true)
-            }
-            prefsItem {
-                val minutes = stringResource(id = R.string.minutes)
-                DropDownPref(
-                    key = AppConstants.SettingsKey.KeepScreenOn,
-                    title = stringResource(id = R.string.keep_screen_on),
-                    textColor = MaterialTheme.colorScheme.primary,
-                    useSelectedAsSummary = true,
-                    entries = mapOf(
-                        "0" to stringResource(id = R.string.follow_system),
-                        "1" to "1 $minutes",
-                        "2" to "2 $minutes",
-                        "3" to "3 $minutes",
-                        "4" to "4 $minutes",
-                        "5" to "5 $minutes",
-                        "10" to "10 $minutes",
-                        "20" to "20 $minutes",
-                        "30" to "30 $minutes"
+                prefsItem{
+                    CheckBoxPref(key = AppConstants.SettingsKey.DisableMovePdf,
+                        title = stringResource(id = R.string.disable_move_pdf),
+                        summary = stringResource(id = R.string.disable_move_pdf_desc),
+                        textColor = MaterialTheme.colorScheme.primary,
+                        defaultChecked = true,
+                        onCheckedChange = {v-> viewModel.disableMovePdf = v},
+                        enabled = true)
+                }
+                prefsItem {
+                    val minutes = stringResource(id = R.string.minutes)
+                    val minute = stringResource(id = R.string.minute)
+                    DropDownPref(
+                        key = AppConstants.SettingsKey.KeepScreenOn,
+                        title = stringResource(id = R.string.keep_screen_on),
+                        textColor = MaterialTheme.colorScheme.primary,
+                        useSelectedAsSummary = true,
+                        entries = mapOf(
+                            "0" to stringResource(id = R.string.follow_system),
+                            "1" to "1 $minute",
+                            "2" to "2 $minutes",
+                            "3" to "3 $minutes",
+                            "4" to "4 $minutes",
+                            "5" to "5 $minutes",
+                            "10" to "10 $minutes",
+                            "20" to "20 $minutes",
+                            "30" to "30 $minutes"
                         ),
-                    defaultValue = "0",
-                    onValueChange = {
-                        viewModel.screenOn.value = it.toInt()
-                    }
-                )
-            }
-            prefsItem {
-                DropDownPref(
-                    key = AppConstants.SettingsKey.Theme,
-                    title = stringResource(id = R.string.theme),
-                    textColor = MaterialTheme.colorScheme.primary,
-                    useSelectedAsSummary = true,
-                    entries = ThemeEnum.values()
-                        .associate { Pair(it.name, stringResource(id = it.resId)) },
-                    defaultValue = ThemeEnum.Default.name,
-                    onValueChange = {
-                        viewModel.theme.value = ThemeEnum.valueOf(it)
-                    }
-                )
-            }
+                        defaultValue = "0",
+                        onValueChange = {
+                            viewModel.screenOn.value = it.toInt()
+                        }
+                    )
+                }
+                prefsItem {
+                    DropDownPref(
+                        key = AppConstants.SettingsKey.Theme,
+                        title = stringResource(id = R.string.theme),
+                        textColor = MaterialTheme.colorScheme.primary,
+                        useSelectedAsSummary = true,
+                        entries = ThemeEnum.values()
+                            .associate { Pair(it.name, stringResource(id = it.resId)) },
+                        defaultValue = ThemeEnum.Default.name,
+                        onValueChange = {
+                            viewModel.theme.value = ThemeEnum.valueOf(it)
+                        }
+                    )
+                }
 
-            prefsItem{
-                TextPref(
-                    title = stringResource(id = R.string.clear_book_cache),
-                    summary =stringResource(id = R.string.clear_all_book_cache_desc),
-                    textColor = MaterialTheme.colorScheme.primary,
-                    onClick = {
-                        scope.launch { CacheManager.delAllCachedPages(ctx) }
-                    }
-                )
+                prefsItem{
+                    val done = stringResource(R.string.done)
+                    TextPref(
+                        title = stringResource(id = R.string.clear_book_cache),
+                        summary = stringResource(id = R.string.clear_all_book_cache_desc),
+                        textColor = MaterialTheme.colorScheme.primary,
+                        enabled = true,
+                        onClick = {
+                            scope.launch {
+                                CacheManager.delAllCachedPages(ctx)
+                                snackState.showSnackbar(done,duration = SnackbarDuration.Short)
+                            }
+                        }
+                    )
+                }
             }
+        }
+
+        SnackbarHost(hostState = snackState){
+            Snackbar(snackbarData = it, Modifier.wrapContentSize(Alignment.Center),
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer)
         }
     }
 }
